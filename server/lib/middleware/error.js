79ba4c0f2137f74f5b6e563e3f86dbfddfbdbb45
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-12-02 14:45:54 -0800

    error

---
+++ server/lib/middleware/error.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+module.exports = (err, req, res, next) => {
+  let status = err.status || 500;
+
+  if(err instanceof mongoose.Error.ValidationError || err instanceof mongoose.Error.CastError) {
+    status = 400;
+  }
+
+  res.status(status);
+
+  // eslint-disable-next-line no-console
+  console.log(err);
+
+  res.send({
+    status,
+    message: err.message
+  });
+};
