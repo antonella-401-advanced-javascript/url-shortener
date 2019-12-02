0475931fa6ff549333bb007481192e175c5f2b3e
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-12-02 10:07:20 -0800

    setting up User component

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+
+      </Switch>
+    </Router>
+  );
+}
---
+++ src/components/user/User.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const User = ({ buttonText, handleSubmit }) => {
+  const sessionId = useSelector(getSessionId);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  if(sessionId) return <Redirect to="/" />;
+
+  return (
+    <form onSubmit={event => handleSubmit(event, username, password)}>
+      <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
+      <input type="password" value={password} onChange={({ target }) => setPassword(target.value)} />
+      <button>{buttonText}</button>
+    </form>
+  );
+};
+
+User.propTypes = {
+  buttonText: PropTypes.string.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+};
+
+export default User;
