037490118f0f3bff77657b0180bbef09ca6a5869
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-12-02 10:22:01 -0800

    auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,1 +1,8 @@
 import { post, get } from './request';
+
+const AUTH_URL = 'http://localhost:7891/api/v1/auth';
+
+export const signup = (username, password) => post(`${AUTH_URL}/signup`, { username, password });
+export const login = (username, password) => post(`${AUTH_URL}/signin`, { username, password });
+
+export const verifySession = () => get(`${AUTH_URL}/verify`);
