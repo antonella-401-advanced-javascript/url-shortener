7e7e702ed91b244c03b0bae4a7609dbaf81249bf
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-12-02 11:13:45 -0800

    working on issue

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect
 } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getSessionId, getSessionLoading } from '../selectors/sessionSelectors';
+import { sessionVerify } from '../actions/sessionActions';
+import SignupUser from '../containers/SignupUser';
+
+const PrivateRoute = ({ ...rest }) => {
+  const sessionId = useSelector(getSessionId);
+  const loading = useSelector(getSessionLoading);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if(!sessionId) dispatch(sessionVerify());
+  }, []);
+
+  if(!loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if(!loading && !sessionId) {
+    return <Redirect to="/login"/>;
+  }
+
+  return <Route {...rest}/>;
+};
 
 export default function App() {
   return (
     <Router>
       <Switch>
-
+        <PrivateRoute path="/login" component={SignupUser} />
       </Switch>
     </Router>
   );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import session from './sessionReducer';
 
 export default combineReducers({
-  session
+  session,
 });
